84a08dfeb201b3e7a658a0cfca962925d4b7c095
João Paulo <jopaulo.as8@gmail.com>
2025-10-15 15:11:43 -0300

    feat: Add tailwind css package for react native

--- mobile/controle-financeiro-app/App.tsx
+++ mobile/controle-financeiro-app/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Paulo Gostoso</Text>
       <StatusBar style="auto" />
     </View>
   );
